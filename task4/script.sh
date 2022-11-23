55b2a3c70f347ddbc22c0bab767011880d91fc66
Your Name <mrzhenshen@gmail.com>
2022-11-23 21:59:20 +0200

    done task#4

--- task4/script.sh
+++ task4/script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-sudo useradd -p $(openssl passwd -6 -salt xyz adminuser) adminuser
+sudo useradd -p $(openssl passwd -6 -salt xyz adminuser) -m adminuser
 sudo usermod -aG sudo adminuser
-
-sudo useradd poweruser
+sudo useradd -m poweruser
 sudo passwd -d poweruser
 echo "poweruser ALL=(ALL:ALL) /usr/sbin/iptables" >> /etc/sudoers
-# setfacl -m g:poweruser:rx /home/adminuser
 sudo usermod -aG adminuser poweruser
 ln -s /home/poweruser/etc/mtab softlink
