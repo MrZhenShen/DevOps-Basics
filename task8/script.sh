272a103c3c531cb7872e06d8a61109041ec40d1f
Your Name <mrzhenshen@gmail.com>
2022-12-07 23:12:53 +0200

    done task8

--- task8/script.sh
+++ task8/script.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 yum -y update
-yum -y install httpd mod_ssl
-yum install firewalld
-
-mkdir /var/www/zhen.com
+yum -y install httpd mod_ssl firewalld
 
 cp -f /vagrant/www-content/index.html /usr/share/httpd/noindex/index.html
+cp -f /vagrant/localhost.conf /etc/httpd/conf.d/
 
 systemctl enable firewalld
 systemctl start firewalld
 firewall-cmd --state
 
-systemctl start httpd
 systemctl enable httpd
-
-cp -f /vagrant/localhost.conf /etc/httpd/conf.d/
+systemctl start httpd
 
 apachectl configtest
 systemctl reload httpd
@@ -25,4 +21,4 @@ firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=443/tcp
 firewall-cmd --reload
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/apache-selfsigned.key -out /etc/pki/tls/certs/apache-selfsigned.crt -subj "/C=UA/ST=Lvivska/L=Lviv/O=ITStep/OU=University/CN=127.0.0.1"
+openssl req -x509 -nodes -days 365 -new -newkey rsa:2048 -keyout /etc/pki/tls/private/apache-selfsigned.key -out /etc/pki/tls/certs/apache-selfsigned.crt
